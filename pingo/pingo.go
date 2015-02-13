721b763dfa600d594c0da753235bc339ae7462eb
Ryan Case <rmcase@gmail.com>
2015-02-13 13:58:08 -0700

    Added checksum and packet construction to the ping program

--- pingo/pingo.go
+++ pingo/pingo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -8,11 +9,14 @@ import (
 	"os"
 )
 
+const (
+	ICMP_TYPE = 8 // 8 = Echo request
+	ICMP_CODE = 0 // 0 = Echo code
+)
+
 func main() {
 	flag.Parse()
-
 	straddr := flag.Arg(0)
-	pid := os.Getpid()
 
 	raddr, err := net.ResolveIPAddr("ip4:icmp", straddr)
 	checkErr(err)
@@ -20,8 +24,14 @@ func main() {
 	conn, err := net.DialIP("ip4:icmp", nil, raddr)
 	checkErr(err)
 
+	pid := uint16(os.Getpid())
+	var seq uint16 = 1
+
+	p, err := makePacket(pid, seq, []byte("Hello, world!"))
+	checkErr(err)
+
 	fmt.Println(conn)
-	fmt.Println(pid)
+	fmt.Println(p)
 }
 
 func checkErr(err error) {
@@ -30,22 +40,52 @@ func checkErr(err error) {
 	}
 }
 
-func makePacket(id, seq int, payload []byte) []byte {
+func makePacket(id, seq uint16, payload []byte) ([]byte, error) {
+	// Find the payload length and ensure it does not exceed the max
 	paylen := len(payload)
+	if paylen > 256 {
+		return nil, errors.New("Payload cannot exceed 256 bytes")
+	}
+
+	// Set up the packet
 	p := make([]byte, 8+paylen)
+	p[0] = ICMP_TYPE         // Type
+	p[1] = ICMP_CODE         // Code
+	p[2] = 0                 // Header checksum - set later
+	p[3] = 0                 // Header checksum - set later
+	p[4] = uint8(id >> 8)    // Identifier
+	p[5] = uint8(id & 0xff)  // Identifier
+	p[6] = uint8(seq >> 8)   // Sequence number
+	p[7] = uint8(seq & 0xff) // Sequence number
 
-	p[0] = 8 // Type
-	p[1] = 0 // Code
-	p[2] = 0 // Header checksum
-	p[3] = 0 // Header checksum
-	p[4] = 0 // Identifier
-	p[5] = 0 // Identifier
-	p[6] = 0 // Sequence number
-	p[7] = 0 // Sequence number
+	// Calculate and add the checksum
+	c := checksum(p)
+	p[2] = uint8(c >> 8)
+	p[3] = uint8(c & 0xff)
 
+	// Add the payload
 	for i := 0; i < paylen; i++ {
 		p[8+i] = payload[i]
 	}
 
-	return p
+	return p, nil
+}
+
+func checksum(p []byte) uint16 {
+	// http://www.faqs.org/rfcs/rfc1071.html
+	cklen := len(p)
+	s := uint32(0)
+
+	for i := 0; i < (cklen - 1); i += 2 {
+		s += uint32(p[i+1])<<8 | uint32(p[i])
+	}
+
+	if cklen&1 == 1 {
+		s += uint32(p[cklen-1])
+	}
+
+	s = (s >> 16) + (s & 0xffff)
+	s = s + (s >> 16)
+
+	return uint16(s)
 }
